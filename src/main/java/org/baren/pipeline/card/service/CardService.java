79d288eb0fe35aeaef9312c690383b701e15e5d2
BarendNoordhoff <BJP.Noordhoff@student.han.nl>
2025-04-28 17:08:29 +0200

    initial commit

---
+++ src/main/java/org/baren/pipeline/card/service/CardService.java
@@ -0,0 +1,21 @@
+package org.baren.pipeline.card.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.baren.pipeline.model.Card;
+import org.baren.pipeline.model.CardType;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CardService {
+    public Card getCard() {
+        Card teval = new Card();
+        teval.setCardName("Teval, the balanced scale");
+        teval.setId(1);
+        teval.setCardType(CardType.CREATURE);
+        teval.setManaValue(4);
+        return teval;
+    }
+}
